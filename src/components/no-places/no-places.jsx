aa44a49fbd0ff278113584dcb8ae693e2514275d
David <dm.dsgnr@gmail.com>
2020-07-17 12:53:18 +0300

    Дорабатывает NoPlaces и убирает лишний memo

--- src/components/no-places/no-places.jsx
+++ src/components/no-places/no-places.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const NoPlaces = () => {
+const NoPlaces = (props) => {
+  const {city} = props;
+
   return <div className="cities__places-container cities__places-container--empty container">
     <section className="cities__no-places">
       <div className="cities__status-wrapper tabs__content">
         <b className="cities__status">No places to stay available</b>
-        <p className="cities__status-description">We could not find any property availbale at the moment in Dusseldorf</p>
+        <p className="cities__status-description">We could not find any property availbale at the moment in {city}</p>
       </div>
     </section>
     <div className="cities__right-section"></div>
   </div>;
+};
+
+NoPlaces.propTypes = {
+  city: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    coordinates: PropTypes.array.isRequired,
+  }).isRequired,
 };
 
 export default NoPlaces;
